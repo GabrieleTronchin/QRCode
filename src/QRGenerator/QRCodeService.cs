77d62c1e3c7846379123ec3512a583e692352be3
Gabriele Tronchin <gabriele.tronchin@reti.it>
2024-07-03 11:08:45 +0200

    Fix windows only support

--- src/QRGenerator/QRCodeService.cs
+++ src/QRGenerator/QRCodeService.cs
@@ -1,6 +1,4 @@
-﻿using System.Drawing;
-using System.Drawing.Imaging;
-using System.IO.Compression;
+﻿using System.IO.Compression;
 using Microsoft.Extensions.Logging;
 using QRCoder;
 using SkiaSharp;
@@ -63,12 +61,15 @@ public class QRCodeService(ILogger<QRCodeService> logger) : IQRCodeService
 
                 for (int i = images.Count - 1; i >= 0; i--)
                 {
-                    using MemoryStream stream = new();
-                    stream.Write(images[i], 0, images[i].Length);
-                    var image2 = Image.FromStream(stream);
+                    using MemoryStream stream = new MemoryStream(images[i]);
+                    stream.Position = 0;
+
+                    var skImage = SKImage.FromEncodedData(stream) ?? throw new InvalidOperationException("Failed to decode image.");
+
                     var filePath = Path.GetTempPath() + $"\\QRCode_{i}.png";
                     files.Add(filePath);
-                    image2.Save(filePath, ImageFormat.Png);
+
+                    SaveImageToFile(skImage, filePath);
                 }
 
                 foreach (var imagePath in files)
@@ -81,22 +82,28 @@ public class QRCodeService(ILogger<QRCodeService> logger) : IQRCodeService
                     await fileStream.CopyToAsync(entryStream);
                 }
 
-                foreach (var item in files)
+                foreach (var file in files)
                 {
-                    File.Delete(item);
+                    File.Delete(file);
                 }
             }
 
             zipFileMemoryStream.Seek(0, SeekOrigin.Begin);
 
             return zipFileMemoryStream.ToArray();
-            // use stream as needed
         }
         catch (Exception ex)
         {
             _logger.LogError(ex, $"An error occurred at {nameof(GenerateQRCodeByte)}");
             throw;
         }
+    }
+
+    private void SaveImageToFile(SKImage imageToSave, string filePath)
+    {
+        using var encodedImage = imageToSave.Encode();
+        using var fileSteam = new FileStream(filePath, FileMode.Create, FileAccess.Write);
+        encodedImage.SaveTo(fileSteam);
     }
 
     private byte[] GenerateDefaultImage(string error)
